dd32625f0a40d453057e8c7b35010e7405e24988
Thomas <thomas.carbillet@gmail.com>
2021-06-02 14:27:15 +0200

    feat: release version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="django-library",
-    version="0.1.0",
+    version="0.1.1",
     description="Handle login and ticket validation for french library connectors (Archimed, GMInvent and C3RB)",
     url="https://github.com/briefmnews/django-library",
     author="Brief.me",
